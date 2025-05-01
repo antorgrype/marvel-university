e784a936ee6c7945d406de9ef6a35aabc9d7cfd8
antorgrype <antor@grype.ca>
2025-05-01 19:40:24 +0600

    archive page added

--- archive.php
+++ archive.php
@@ -3,9 +3,9 @@ get_header(); ?>
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Welcome to our blog</h1>
+    <h1 class="page-banner__title"><?= the_archive_title() ?></h1>
     <div class="page-banner__intro">
-      <p>Keep up with our latest news</p>
+      <p><?= the_archive_description(); ?></p>
     </div>
   </div>
 </div>
