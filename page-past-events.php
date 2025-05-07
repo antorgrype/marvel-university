6104e545e0c40c7ce78bb0e7c3fd3fbad68ef646
antorgrype <antor@grype.ca>
2025-05-07 23:29:28 +0600

    aaded pagination on custom query

--- page-past-events.php
+++ page-past-events.php
@@ -3,17 +3,35 @@ get_header(); ?>
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Events</h1>
+        <h1 class="page-banner__title">Past Events</h1>
         <div class="page-banner__intro">
-            <p>Know what's happening in the world</p>
+            <p>Know what happened previously in the world</p>
         </div>
     </div>
 </div>
 
 <div class="container container--narrow page-section">
     <?php
-    while (have_posts()) {
-        the_post(); 
+    $today = date('Ymd');
+    $pastEvents = new WP_Query([
+        'paged' => get_query_var('paged', 1),
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'meta_query' => [
+            [
+                'key' => 'event_date',
+                'compare' => '<',
+                'value' => $today,
+            ]
+        ],
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+    ]);
+
+
+
+    while ($pastEvents->have_posts()) {
+        $pastEvents->the_post();
         $eventDate = new DateTime(get_field('event_date'));
         $month = $eventDate->format('M');
         $day = $eventDate->format('d');
@@ -26,18 +44,18 @@ get_header(); ?>
             <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a
                         href="<?= the_permalink(); ?>"><?= the_title(); ?></a></h5>
-                <p><?= has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?> <a href="<?= the_permalink(); ?>" class="nu gray">Learn
+                <p><?= has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?> <a
+                        href="<?= the_permalink(); ?>" class="nu gray">Learn
                         more</a></p>
             </div>
         </div>
         <?php
     }
 
-    echo paginate_links();
+    echo paginate_links([
+        'total' => $pastEvents->max_num_pages,
+    ]);
     ?>
-
-    <hr class="section-break">
-    <p>Looking for a past event? <a href="<?php echo site_url('/past-events') ?>">Checkout all the pasts events you missed</a></p>
 </div>
 
 <?php
